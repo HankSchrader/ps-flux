76dbe320d1ea8bc4ce457f8a9ed8d8fef4450109
Erik Mikac <mikacew@gmail.com>
2020-03-14 13:35:40 -0400

    Added AboutPage.js and HomePage.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "bootstrap/dist/css/bootstrap-grid.min.css"
+import React from "react";
+import { render } from 'react-dom'
+import HomePage from "./components/HomePage";
+import AboutPage from "./AboutPage";
+
+render(<HomePage/>, document.getElementById('root'));
+
